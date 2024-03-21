036872fd3a098eb8d1e8aac02eb873ea0b2583a6
Łukasz Kapica <lukaszkapica@gmail.com>
2024-03-21 19:45:13 +0100

    Add brand new page

---
+++ resources/js/Pages/BrandNewPage.jsx
@@ -0,0 +1,5 @@
+export default function BrandNewPage() {
+    return (
+        <h1>Brand New Page</h1>
+    )
+}
--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/brandnewpage', function () {
+    return inertia('BrandNewPage');
+});
+
 require __DIR__.'/auth.php';
